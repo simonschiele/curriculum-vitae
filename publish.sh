92330f8fbfe0fb5b2d906b1d5cfc54fa10023e9c
Simon Schiele <simon.codingmonkey@gmail.com>
2020-07-17 14:10:00 +0200

    Add publish.sh to update public CV exports

---
+++ publish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+scp publish/simon_schiele_cv_english.pdf simon@psaux.de:/srv/www/simon/htdocs/files/simon_schiele_cv_2020_english.pdf
+scp publish/simon_schiele_cv_german.pdf simon@psaux.de:/srv/www/simon/htdocs/files/simon_schiele_cv_2020_german.pdf
